83a13ee0f075e37669a409cc8eb0d6027dad1b05
Anatole <me@anatole.dev>
2021-08-17 16:30:21 +0200

    Fill footer bg on home page / minimize otherwise

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,26 @@
 <template>
-	<footer>
+	<footer :class="$route.path === '/' ? 'filled' : ''">
 		<div class="breadcrumbs">
-			<router-link to="/contact" class="crumb">Contact</router-link>
-			<font-awesome-icon :icon="['fas', 'circle']" class="splitter" />
-			<a href="https://github.com/SevenTV" class="crumb">GitHub</a>
-			<font-awesome-icon :icon="['fas', 'circle']" class="splitter" />
-			<router-link to="/privacy" class="crumb blue">Privacy Policy</router-link>
-			<font-awesome-icon :icon="['fas', 'circle']" class="splitter" />
-			<router-link to="/tos" class="crumb blue">Terms of Service</router-link>
-			<font-awesome-icon :icon="['fas', 'circle']" class="splitter" />
-			<router-link to="/dmca" class="crumb blue">DMCA</router-link>
+			<div>
+				<router-link to="/contact" class="crumb">Contact</router-link>
+				<font-awesome-icon :icon="['fas', 'circle']" class="splitter" />
+				<a href="https://github.com/SevenTV" class="crumb">GitHub</a>
+				<font-awesome-icon :icon="['fas', 'circle']" class="splitter" />
+				<router-link to="/privacy" class="crumb blue">Privacy Policy</router-link>
+				<font-awesome-icon :icon="['fas', 'circle']" class="splitter" />
+				<router-link to="/tos" class="crumb blue">Terms of Service</router-link>
+				<font-awesome-icon :icon="['fas', 'circle']" class="splitter" />
+				<router-link to="/dmca" class="crumb blue">DMCA</router-link>
+			</div>
 		</div>
 	</footer>
 </template>
+
+<script lang="ts">
+export default {
+	props: ["footerFilled"],
+};
+</script>
 
 <style lang="scss" scoped>
 @import "@/assets/scss/footer.scss";
