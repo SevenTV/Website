c7ae3b021e0f31cf1a6671cc92b06b07ba73ca50
TroyDota <troybensonsa@gmail.com>
2021-08-17 02:52:08 +0200

    feat: emote menu

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import store from "@/store";
 import { createHead } from "@vueuse/head";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSun as fasSun, faMoon as fasMoon, faCircle as fasCircle, faBars as fasBars } from "@fortawesome/free-solid-svg-icons";
+import { faSun as fasSun, faMoon as fasMoon, faCircle as fasCircle, faBars as fasBars, faPlus as fasPlus } from "@fortawesome/free-solid-svg-icons";
 import { faChrome as fabChrome, faTwitter as fabTwitter, faGithub as fabGithub } from "@fortawesome/free-brands-svg-icons";
 
-library.add(fasSun, fasMoon, fabChrome, fabTwitter, fabGithub, fasCircle, fasBars);
+library.add(fasSun, fasMoon, fabChrome, fabTwitter, fabGithub, fasCircle, fasBars, fasPlus);
 
 const app = createApp(App);
 
