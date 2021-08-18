01e3fd979933c161e1a15ecce5ff879494a1b5f5
Anatole <me@anatole.dev>
2021-08-18 18:49:52 +0200

    Set up Apollo Client (GQL)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp } from "vue";
+import { createApp, h, provide } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
@@ -7,9 +7,31 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSun as fasSun, faMoon as fasMoon, faCircle as fasCircle, faBars as fasBars, faPlus as fasPlus } from "@fortawesome/free-solid-svg-icons";
 import { faChrome as fabChrome, faTwitter as fabTwitter, faGithub as fabGithub, faTwitch as fabTwitch } from "@fortawesome/free-brands-svg-icons";
+import { DefaultApolloClient } from "@vue/apollo-composable";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client/core";
 
 library.add(fasSun, fasMoon, fabChrome, fabTwitter, fabGithub, fasCircle, fasBars, fasPlus, fabTwitch);
 
-const app = createApp(App);
+// API GQL Endpoint
+const httpLink = createHttpLink({
+	uri: "http://localhost:3000/v2/gql",
+});
+
+// Set cache
+const cache = new InMemoryCache();
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+	link: httpLink,
+	cache,
+});
+
+const app = createApp({
+	setup() {
+		provide(DefaultApolloClient, apolloClient);
+	},
+
+	render: () => h(App),
+});
 
 app.use(createHead()).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
