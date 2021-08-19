e2ab51218e422e8dbb944297893f8d4183b87d59
Anatole <me@anatole.dev>
2021-08-19 04:14:09 +0200

    GQL - Apollo Client (#18)
    * Set up Apollo Client (GQL)
    
    * Split apollo client into own file
    
    * dev/stage env

---
+++ src/apollo.ts
@@ -0,0 +1,16 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client/core";
+
+// HTTP connection to the API
+const httpLink = createHttpLink({
+	// You should use an absolute URL here
+	uri: `${process.env.VUE_APP_API}/v2/gql`,
+});
+
+// Cache implementation
+const cache = new InMemoryCache();
+
+// Create the apollo client
+export const apolloClient = new ApolloClient({
+	link: httpLink,
+	cache,
+});
