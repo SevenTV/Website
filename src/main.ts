f8150d1b41e78fdf0667baeb2cf9a5bc780737e7
Anatole <me@anatole.dev>
2021-08-19 04:08:33 +0200

    Split apollo client into own file

--- src/main.ts
+++ src/main.ts
@@ -8,23 +8,9 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSun as fasSun, faMoon as fasMoon, faCircle as fasCircle, faBars as fasBars, faPlus as fasPlus } from "@fortawesome/free-solid-svg-icons";
 import { faChrome as fabChrome, faTwitter as fabTwitter, faGithub as fabGithub, faTwitch as fabTwitch } from "@fortawesome/free-brands-svg-icons";
 import { DefaultApolloClient } from "@vue/apollo-composable";
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client/core";
+import { apolloClient } from "@/apollo";
 
 library.add(fasSun, fasMoon, fabChrome, fabTwitter, fabGithub, fasCircle, fasBars, fasPlus, fabTwitch);
-
-// API GQL Endpoint
-const httpLink = createHttpLink({
-	uri: "http://localhost:3000/v2/gql",
-});
-
-// Set cache
-const cache = new InMemoryCache();
-
-// Create the apollo client
-const apolloClient = new ApolloClient({
-	link: httpLink,
-	cache,
-});
 
 const app = createApp({
 	setup() {
