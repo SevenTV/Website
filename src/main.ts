60b13d32b77983cbe302ead4c64e808a5c7e7b4a
Anatole <me@anatole.dev>
2021-08-19 04:16:25 +0200

    Reset apollo client impl. (done by #18)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { createApp, h, provide } from "vue";
+import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
@@ -7,16 +7,9 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faSun as fasSun, faMoon as fasMoon, faCircle as fasCircle, faBars as fasBars, faPlus as fasPlus } from "@fortawesome/free-solid-svg-icons";
 import { faChrome as fabChrome, faTwitter as fabTwitter, faGithub as fabGithub, faTwitch as fabTwitch } from "@fortawesome/free-brands-svg-icons";
-import { DefaultApolloClient } from "@vue/apollo-composable";
-import { apolloClient } from "@/apollo";
 
 library.add(fasSun, fasMoon, fabChrome, fabTwitter, fabGithub, fasCircle, fasBars, fasPlus, fabTwitch);
 
-const app = createApp({
-	setup() {
-		provide(DefaultApolloClient, apolloClient);
-	},
-	render: () => h(App),
-});
+const app = createApp(App);
 
 app.use(createHead()).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
