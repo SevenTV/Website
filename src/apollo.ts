b83a2ca9c35eea02e9e32fb0d5abb615d6139448
Anatole <me@anatole.dev>
2021-09-09 07:27:15 +0200

    apollo - persisted query

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,10 +1,17 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client/core";
+import { createPersistedQueryLink } from "@apollo/client/link/persisted-queries";
+import { sha256 } from "crypto-hash";
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
 	// You should use an absolute URL here
-	uri: `${process.env.VUE_APP_API}/v2/gql`,
-});
+	uri: `${process.env.VUE_APP_API}/v3/gql`,
+}).concat(
+	createPersistedQueryLink({
+		sha256,
+		useGETForHashedQueries: true,
+	})
+);
 
 // Cache implementation
 const cache = new InMemoryCache();
