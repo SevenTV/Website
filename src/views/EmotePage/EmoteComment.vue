3d2e95f4fee18a68cc2fc31bc0ef08e271b5a5e9
Sirinoks <shinigamoo@gmail.com>
2021-10-28 06:29:55 -0400

    View established
    Basic stuff added to make the view work

---
+++ src/views/EmotePage/EmoteComment.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="comment">
+		test
+		<div class="time">42.56</div>
+		<UserTag scale="1em">Siri</UserTag>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import UserTag from "@/components/utility/UserTag.vue";
+
+export default defineComponent({
+	components: {
+		UserTag,
+	},
+});
+</script>
