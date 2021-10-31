ce37e50035f66e9b972474e223034594198ada72
Sirinoks <shinigamoo@gmail.com>
2021-10-31 10:35:55 -0400

    Comment styling added

--- src/views/EmotePage/EmoteComment.vue
+++ src/views/EmotePage/EmoteComment.vue
@@ -1,8 +1,15 @@
+/* eslint-disable prettier/prettier */
 <template>
 	<div class="comment">
-		test
-		<div class="time">42.56</div>
-		<UserTag scale="1em">Siri</UserTag>
+		<UserTag scale="1em"></UserTag>
+		<div class="comment-text">
+			Anyways uhm... I bought a whole bunch of shungite, rocks, do you know what shungite is? Anybody know what
+			shungite is. No, no Suge Knight, I think hes locked up in prison. Talking shungite. Anyways, its a 2 billion
+			year old like rock, stone that protects against frequencies and unwanted frequencies that may be traveling
+			in the air. So thats my story. I bought a whole bunch of stuff, put them around the la casa. Little
+			pyramids. Stuff like that.
+		</div>
+		<div class="time">03.09.2018, 19:02</div>
 	</div>
 </template>
 
@@ -16,3 +23,8 @@ export default defineComponent({
 	},
 });
 </script>
+
+<style lang="scss" scoped>
+@import "@scss/emotes.scss";
+@import "@scss/components/user-tag.scss";
+</style>
