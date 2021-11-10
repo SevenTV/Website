da98111579ef6516a89040b3318e6603f95bfd64
Anatole <me@anatole.dev>
2021-11-10 14:03:03 +0100

    French locale

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,8 +1,10 @@
 import { createI18n } from "vue-i18n";
 import { en_US } from "./lang/en_US";
+import { fr_FR } from "./lang/fr_FR";
 
 export const messages = {
 	en_US,
+	fr_FR,
 };
 
 export const i18n = createI18n({
