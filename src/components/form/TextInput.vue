fe34dd13f95f5c7534de02d5b251118f001302d1
Anatole <me@anatole.dev>
2021-11-17 14:10:05 +0100

    Form Controls (#151)
    * TextInput
    
    * Checkbox
    
    * Checkbox v-model
    
    * Radio

---
+++ src/components/form/TextInput.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="text-input">
+		<input :value="modelValue" @input="onInput" />
+		<label>
+			<span> {{ label }} </span>
+		</label>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue-demi";
+
+export default defineComponent({
+	props: {
+		label: String,
+		modelValue: String,
+	},
+	setup(props, { emit }) {
+		const onInput = (event: Event) => emit("update:modelValue", (event.target as HTMLInputElement).value);
+
+		return {
+			onInput,
+		};
+	},
+});
+</script>
+
+<style lang="scss">
+@import "@scss/components/form/text-input.scss";
+</style>
