9371b8d56671c3c03114071c75e7d1c1d1c95a36
Anatole <me@anatole.dev>
2022-02-17 11:28:52 +0100

    apollo - Updated auth

--- src/apollo/index.ts
+++ src/apollo/index.ts
@@ -1,5 +1,8 @@
-import { ApolloClient, createHttpLink, InMemoryCache, ApolloLink } from "@apollo/client/core";
+import { ApolloLink, ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client/core";
 import { WebSocketLink } from "@apollo/client/link/ws";
+import { split } from "@apollo/client/core";
+import { SubscriptionClient } from "subscriptions-transport-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
@@ -8,19 +11,37 @@ const httpLink = createHttpLink({
 });
 
 // WebSocket Transport
-const wsLink = new WebSocketLink({
-	uri: `${import.meta.env.VITE_APP_API_GQL_WS}`,
-	options: {
-		reconnect: true,
-		connectionParams: {
-			Authorization: `Bearer ${localStorage.getItem("token")}`,
-		},
+let wsConnected = false;
+export const wsClient = new SubscriptionClient(`${import.meta.env.VITE_APP_API_GQL_WS}`, {
+	reconnect: true,
+	connectionParams: () => {
+		const tkn = localStorage.getItem("token");
+		if (tkn) {
+			return {
+				Authorization: `Bearer ${tkn}`,
+			};
+		}
 	},
 });
+
+const wsLink = new WebSocketLink(wsClient);
+
+const splitLink = split(
+	({ query }) => {
+		const def = getMainDefinition(query);
+		return wsConnected || (def.kind === "OperationDefinition" && def.operation === "subscription");
+	},
+	wsLink,
+	httpLink
+);
+
+wsClient.onConnected(() => (wsConnected = true));
+wsClient.onDisconnected(() => (wsConnected = false));
 
 // Set up auth
 const authLink = new ApolloLink((op, next) => {
 	const tkn = localStorage.getItem("token");
+
 	if (tkn) {
 		op.setContext({
 			headers: {
@@ -31,12 +52,19 @@ const authLink = new ApolloLink((op, next) => {
 	return next(op);
 });
 
+const link = ApolloLink.from([authLink, splitLink]);
+
+export const reconnect = async () => {
+	wsClient.close(false, true);
+	return new Promise((resolve) => wsClient.onConnected(() => resolve(undefined)));
+};
+
 // Cache implementation
 const cache = new InMemoryCache();
 
 // Create the apollo client
 export const apolloClient = new ApolloClient({
-	link: ApolloLink.from([authLink, wsLink, httpLink]),
+	link: link,
 	cache,
 	defaultOptions: {
 		watchQuery: {
