ccf5c504a984dd91882e129a247650fd40c852be
Anatole <me@anatole.dev>
2022-09-20 09:24:14 +0200

    add random rotation and second layer for graveyard

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,21 +3,29 @@
 		<div ref="contentRef" class="app-footer-content">
 			<!-- Place tombstones -->
 			<div class="graveyard">
-				<div v-for="[x, s, c] of points" :key="x">
-					<component :is="c" :style="{ left: x, width: s }" class="tomb" />
-				</div>
-
-				<div class="fog-area">
-					<Fog />
+				<div v-for="[x, s, rot, c] of points.layer2" :key="x" class="tomblayer" :layer="2">
+					<component :is="c" :style="{ left: x, width: s, transform: `rotate(${rot}deg)` }" class="tomb" />
 				</div>
 			</div>
+
+			<div class="graveyard">
+				<div v-for="[x, s, rot, c] of points.layer1" :key="x" class="tomblayer" :layer="1">
+					<component :is="c" :style="{ left: x, width: s, transform: `rotate(${rot}deg)` }" class="tomb" />
+				</div>
+			</div>
+		</div>
+
+		<div class="fog-area">
+			<Fog />
 		</div>
 	</main>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, markRaw } from "vue";
+import { onMounted, ref, markRaw, reactive } from "vue";
 import type { Component } from "vue";
+import { storeToRefs } from "pinia";
+import { useStore } from "@/store/main";
 import Tomb1 from "./base/halloween/tomb1.vue";
 import Tomb2 from "./base/halloween/tomb2.vue";
 import Tomb3 from "./base/halloween/tomb3.vue";
@@ -33,8 +41,6 @@ import Tomb12 from "./base/halloween/tomb12.vue";
 import Tomb13 from "./base/halloween/tomb13.vue";
 import Tomb14 from "./base/halloween/tomb14.vue";
 import Fog from "./utility/Fog.vue";
-import { storeToRefs } from "pinia";
-import { useStore } from "@/store/main";
 
 const { theme } = storeToRefs(useStore());
 
@@ -54,7 +60,12 @@ const tombstones = [
 	Tomb13,
 	Tomb14,
 ] as Component[];
-const points = ref([] as [number, number, Component][]);
+
+const points = reactive({
+	layer1: [] as [number, number, number, Component][],
+	layer2: [] as [number, number, number, Component][],
+});
+// const points = ref([] as [number, number, number, Component][]);
 
 const contentRef = ref<HTMLDivElement | null>(null);
 
@@ -67,26 +78,31 @@ onMounted(() => {
 	const w = window.innerWidth;
 
 	// Generate tombstones
-	let gen = true;
-	do {
-		// get random tombstone vector
-		const tomb = tombstones[Math.floor(Math.random() * tombstones.length)];
+	for (const layer of [points.layer1, points.layer2]) {
+		let gen = true;
+		do {
+			// get random tombstone vector
+			const tomb = tombstones[Math.floor(Math.random() * tombstones.length)];
 
-		// last point where a tombstone was placed
-		const lastPoint = points.value[points.value.length - 1]?.[0] ?? 0;
+			// last point where a tombstone was placed
+			const lastPoint = layer[layer.length - 1]?.[0] ?? 0;
 
-		// random tomb size
-		const tombSize = Math.floor(Math.random() * (4 * 16)) + 1 * 16;
+			// random tomb size
+			const tombSize = Math.floor(Math.random() * (4 * 16)) + 1 * 16;
 
-		// random x location value starting from previous point
-		const point = lastPoint + tombSize - 25;
+			// random tomb rotation between 22.5 degrees
+			const tombRotation = -5 + Math.floor(Math.random() * 10);
 
-		points.value.push([point, tombSize, markRaw(tomb)]);
+			// random x location value starting from previous point
+			const point = lastPoint + Math.floor(Math.random() * 85);
 
-		if (point >= w - tombSize) {
-			gen = false;
-		}
-	} while (gen);
+			layer.push([point, tombSize, tombRotation, markRaw(tomb)]);
+
+			if (point >= w - tombSize) {
+				gen = false;
+			}
+		} while (gen);
+	}
 });
 </script>
 
@@ -117,6 +133,7 @@ main.app-footer {
 			justify-content: center;
 			align-items: flex-end;
 			background-image: linear-gradient(0deg, black 1em, transparent 0);
+			position: fixed;
 
 			height: 100%;
 			width: 100%;
@@ -124,14 +141,25 @@ main.app-footer {
 			svg {
 				color: black;
 			}
-		}
 
-		.fog-area {
-			position: absolute;
-			opacity: 0.75;
-			height: 100vh;
-			width: 100%;
+			[layer="1"] {
+				z-index: 3;
+			}
+
+			[layer="2"] {
+				svg {
+					opacity: 0.42;
+				}
+			}
 		}
 	}
+}
+
+.fog-area {
+	position: absolute;
+	opacity: 0.75;
+	top: 0;
+	height: 100vh;
+	width: 100%;
 }
 </style>
