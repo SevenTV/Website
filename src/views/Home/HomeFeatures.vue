0e63b36bed73bbd5fc03e9f5f0d0e8ab477d40cf
loczek <30776250+loczek@users.noreply.github.com>
2022-10-27 23:03:45 +0200

    home: improve features ui

--- src/views/Home/HomeFeatures.vue
+++ src/views/Home/HomeFeatures.vue
@@ -10,7 +10,6 @@
 				<h3>{{ t("home.features.many_emote_slots.name") }}</h3>
 				<p>{{ t("home.features.many_emote_slots.detail", [300, 600]) }}</p>
 			</div>
-
 			<div card="channel_emote_names" class="feature-card">
 				<h3>{{ t("home.features.channel_emote_names.name") }}</h3>
 				<p>{{ t("home.features.channel_emote_names.detail") }}</p>
@@ -31,7 +30,6 @@
 				<h3>{{ t("home.features.next_gen.name") }}</h3>
 				<p>{{ t("home.features.next_gen.detail") }}</p>
 			</div>
-
 			<div card="source_available" class="feature-card">
 				<h3>{{ t("home.features.source_available.name") }}</h3>
 				<p>{{ t("home.features.source_available.detail") }}</p>
@@ -58,12 +56,13 @@ const emoteCount = "200,000";
 			border-bottom: 1px solid themed("backgroundColor");
 		}
 		.feature-card {
-			background-color: darken(themed("backgroundColor"), 1);
+			background-color: lighten(themed("backgroundColor"), 3);
 		}
 	}
 
 	> h1 {
-		margin-top: 0.5em;
+		margin-top: 1.5em;
+		margin-bottom: 0.5em;
 		font-size: 2em;
 		text-align: center;
 	}
@@ -74,34 +73,33 @@ const emoteCount = "200,000";
 		flex-wrap: wrap;
 		justify-content: center;
 		flex-basis: 33.33%;
+		margin-bottom: 2em;
 
-		@media screen and (max-width: 1200px) {
+		@media screen and (max-width: 1400px) {
 			grid-template-columns: repeat(3, auto);
 		}
 
-		@media screen and (max-width: 900px) {
+		@media screen and (max-width: 1000px) {
 			grid-template-columns: repeat(2, auto);
 		}
 
-		@media screen and (max-width: 500px) {
+		@media screen and (max-width: 700px) {
 			grid-template-columns: repeat(1, auto);
 		}
 	}
 	.feature-card {
-		padding-top: 1em;
-		clip-path: polygon(5% 0, 95% 1%, 100% 10%, 100% 90%, 95% 100%, 5% 100%, 0 90%, 0 10%);
+		padding: 1.5rem 2em;
+		clip-path: polygon(0% 0, 95% 0%, 100% 10%, 100% 100%, 100% 100%, 0% 100%, 0 100%, 0 0);
 		z-index: 1;
 		margin: 1em;
-		min-height: 12em;
-		width: 16em;
+		min-height: 11em;
+		width: 19em;
 
 		> h3 {
-			margin: 0.5em;
-			font-weight: 300;
-			letter-spacing: 0.05em;
+			font-weight: 500;
 		}
 		> p {
-			margin: 0.75em;
+			margin-top: 0.5em;
 		}
 	}
 }
