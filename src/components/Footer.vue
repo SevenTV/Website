fb777c99ce5b9a6233ea22af4e278edd9dd8b8af
Anatole <me@anatole.dev>
2022-12-01 02:54:21 +0100

    snow footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="footer-christmas">
+		<span class="tiling">
+			<!-- <SnowFloor /> -->
+		</span>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.footer-christmas {
+	position: fixed;
+	pointer-events: none;
+	bottom: 0;
+	width: 100%;
+	height: 4vh;
+
+	background-image: url("@/assets/img/snow.svg");
+	background-repeat: repeat-x;
+	background-size: 9vw;
+}
+</style>
