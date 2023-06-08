cd3004cad21a93f2ba2f05c149b67137207fceea
Anatole <me@anatole.dev>
2023-06-08 17:57:13 +0200

    kick help page

---
+++ src/views/help/Help.vue
@@ -0,0 +1,11 @@
+<template>
+	<main v-if="route.meta.title" class="help">
+		<RouterView />
+	</main>
+</template>
+
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
