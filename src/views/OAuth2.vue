bfc715784643a29eced6f02dc64ef7ed2ed50e1f
Anatole <me@anatole.dev>
2023-06-19 09:50:47 +0200

    kick: intermediate auth callback

--- src/views/OAuth2.vue
+++ src/views/OAuth2.vue
@@ -19,5 +19,38 @@ if (typeof token === "string") {
 	localStorage.setItem(LocalStorageKeys.TOKEN, token);
 }
 
-window.close();
+const manual = route.query.manual;
+
+if (manual) {
+	const platform = route.query.platform;
+	const userID = route.query.user_id;
+	if (!platform || !userID) {
+		throw new Error("missing platform or user_id");
+	}
+
+	// Request the API to verify the code in bio
+	const query = new URLSearchParams([
+		["platform", platform.toString()],
+		["id", userID.toString()],
+		["verify", "1"],
+	]);
+
+	const currentToken = localStorage.getItem(LocalStorageKeys.TOKEN);
+	const resp = await fetch(import.meta.env.VITE_APP_API_REST + `/auth/manual?${query.toString()}`, {
+		headers: {
+			Authorization: `Bearer ${currentToken}`,
+		},
+	});
+	if (!resp || !resp.ok) {
+		throw new Error("failed to verify code");
+	}
+
+	const cb = route.query.callback ? decodeURIComponent(route.query.callback.toString()) : "";
+	const tok = resp.headers.get("X-Access-Token");
+	if (cb && tok) {
+		window.location.href = cb + "?seventv_token=" + tok;
+	}
+}
+
+// window.close();
 </script>
